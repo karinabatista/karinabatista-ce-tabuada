423474f282c86854427beb631d21f28f13dfe22a
karinabatista <karinabatista12@gmail.com>
2017-04-03 20:42:02 -0300

    Primeira Versão - Não Finalizado

---
+++ saida.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+	<head lang="pt-br">
+		<meta charset='utf-8'>
+		<title>Tabuada</title>
+	</head>
+	<body>
+		<?php 
+			$numUm = $_POST['numUm'];
+			$numDois = $_POST['numDois'];
+			$multUm = $_POST['multUm'];
+			$multDois = $_POST['multDois'];
+			
+			
+			$inicio = $numUm;
+			$fim = $numDois;
+			$i = $multUm;
+			$multiplicador = $multDois;
+		
+			for ($i < $fim; $i++) {
+				
+				for ($inicio < $multiplicador; i++){
+					$resultado = $i*$multiplicador;
+					echo "$i X $multiplicador = $resultado<br>";
+				}
+				
+			}
+			
+			/* FUNCIONANDO
+			
+			while ($i < $multUm) {
+				while ($multDois > $multiplicador){
+					$resultado = $multUm * $multiplicador;
+					echo "$multUm X $multiplicador = $resultado<br>";
+					
+					$multiplicador++;
+				}
+				$i++;
+			}*/
+		?>
+	</body>
+
+</html>
